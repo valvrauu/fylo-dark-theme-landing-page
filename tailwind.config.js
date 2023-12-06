0d28d6fe2844d1cf641fbb4cf01741ee707c8b45
Valter Oliveira <ubi00179@gmail.com>
2023-12-06 02:49:45 -0300

    :sparkles: feat: add new box-shadow (dark) in tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,7 @@ module.exports = {
       },
       boxShadow: {
         'light': '5px 5px 8px 4px rgba(56, 56, 56, 0.10)',
+        'dark': '5px 5px 8px 0px rgba(0, 0, 0, 0.25)',
       }
     },
   },
